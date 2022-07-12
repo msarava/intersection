46db7bf5f37c414f5459bb475a15c94e6f272753
Antoine Dabonneville <antoine.dabonneville@pm.me>
2022-07-12 15:43:20 +0200

    terminate test and link frontend

--- src/lib/calculate.intersection.js
+++ src/lib/calculate.intersection.js
@@ -1,12 +1,38 @@
- function calculateIntersection(orderTime1, orderTime2) {
-  
- 
-  //TODO calculer l'intersection
-
-
-
-
-  return 'ok';
+function timeToFloat(TimeString) {
+  return (
+    parseInt(TimeString.split(':')[0]) + parseInt(TimeString.split(':')[1]) / 60
+  );
 }
 
-module.exports= calculateIntersection;
+function timeToString(TimeFloat) {
+  return (
+    Math.trunc(TimeFloat) +
+    ':' +
+    ((TimeFloat - Math.trunc(TimeFloat)) * 60).toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    })
+  );
+}
+
+export default function calculateIntersection(orderTime1, orderTime2) {
+  if (arguments.length < 2) return null;
+  if (
+    Math.max(timeToFloat(orderTime2.start), timeToFloat(orderTime1.end)) >
+      timeToFloat(orderTime2.start) &&
+    Math.max(timeToFloat(orderTime1.start), timeToFloat(orderTime2.start)) <
+      Math.min(timeToFloat(orderTime1.end), timeToFloat(orderTime2.end))
+  ) {
+    return {
+      start: timeToString(
+        Math.max(timeToFloat(orderTime1.start), timeToFloat(orderTime2.start))
+      ),
+
+      end: timeToString(
+        Math.min(timeToFloat(orderTime1.end), timeToFloat(orderTime2.end))
+      ),
+    };
+  } else {
+    return 'No intersection';
+  }
+}
--- src/lib/calculate.intersection.test.js
+++ src/lib/calculate.intersection.test.js
@@ -1,5 +1,38 @@
-const calculateIntersection = require("./calculate.intersection");
+const calculateIntersection = require('./calculate.intersection');
 
+calculateIntersection;
 test('calculateIntersection', () => {
   expect(typeof calculateIntersection).toBe('function');
+  expect(calculateIntersection.length).toBe(2);
+  expect(calculateIntersection()).toBe(null);
+  expect(
+    calculateIntersection(
+      { start: '10:00', end: '14:00' },
+      { start: '11:00', end: '15:00' }
+    )
+  ).toEqual({ start: '11:00', end: '14:00' });
+  expect(
+    calculateIntersection(
+      { start: '11:00', end: '15:00' },
+      { start: '10:00', end: '14:00' }
+    )
+  ).toEqual({ start: '11:00', end: '14:00' });
+  expect(
+    calculateIntersection(
+      { start: '11:00', end: '15:00' },
+      { start: '10:35', end: '14:10' }
+    )
+  ).toEqual({ start: '11:00', end: '14:10' });
+  expect(
+    calculateIntersection(
+      { start: '10:00', end: '14:00' },
+      { start: '14:00', end: '15:00' }
+    )
+  ).toEqual('No intersection');
+  expect(
+    calculateIntersection(
+      { start: '14:00', end: '15:00' },
+      { start: '10:00', end: '12:00' }
+    )
+  ).toEqual('No intersection');
 });
